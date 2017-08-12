1ec954b3ca818c129a34b0752c13958258e41c0c
Murray Tait <death.tumble@gmail.com>
2017-08-12 19:51:41 +0100

    Filled out stub files to cache the inverse of a Matrix
    Required assignment work for Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly 
+## (there are also alternatives to matrix inversion that we will not discuss 
+## here). Your assignment is to write a pair of functions that cache the inverse
+## of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(cached_mat = matrix()) {
+    cached_inv <- NULL
+    
+    setmat <- function(mat) {
+        cached_mat <<- mat
+        inv <<- NULL
+    }
+    
+    getmat <- function() cached_mat
+    
+    setinv <- function(inv) cached_inv <<-inv 
+    
+    getinv <- function() cached_inv
+    
+    list(setmat = setmat,
+         getmat = getmat,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above
 
-## Write a short comment describing this function
+cacheSolve <- function(cache, ...) {
+    inv <- cache$getinv()
+    
+    if(!is.null(inv)) {
+        message("using cache")
+        return(inv)            
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    mat <- cache$getmat()
+    inv <- solve(mat, ...)
+    cache$setinv(inv)
+    
+    inv
 }
